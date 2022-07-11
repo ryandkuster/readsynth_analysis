fe0ba6bd5befe054d9338a104e089d5d009e20c9
ryandkuster <rkuster@utk.edu>
2022-07-11 10:35:01 -0400

    added 100u and 80c simulation analyses

--- analysis/snipen_2021/depth_comparison/run_depth_summary.sh
+++ analysis/snipen_2021/depth_comparison/run_depth_summary.sh
@@ -22,77 +22,77 @@ motif_combine_depths(){
   done
 }
 
-# bwa mem all mappings
-r1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199716
-r1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_1_combined_depths.tsv
-combine_depths $r1 $r1_out
-
-r2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199724
-r2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_2_combined_depths.tsv
-combine_depths $r2 $r2_out
-
-r3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199725
-r3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_3_combined_depths.tsv
-combine_depths $r3 $r3_out
-
-
-
-s1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199716_sim
-s1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_1_combined_depths.tsv
-combine_depths $s1 $s1_out
-
-s2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199724_sim
-s2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_2_combined_depths.tsv
-combine_depths $s2 $s2_out
-
-s3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199725_sim
-s3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_3_combined_depths.tsv
-combine_depths $s3 $s3_out
-
-
-
-smsd1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199716_sim
-smsd1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_1_combined_depths.tsv
-combine_depths $smsd1 $smsd1_out
-
-smsd2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199724_sim
-smsd2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_2_combined_depths.tsv
-combine_depths $smsd2 $smsd2_out
-
-smsd3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199725_sim
-smsd3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_3_combined_depths.tsv
-combine_depths $smsd3 $smsd3_out
-
-
-# bwa mem matchings with motif sites only
-r1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199716
-r1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_1_combined_depths.tsv
-motif_combine_depths $r1 $r1_out
-
-r2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199724
-r2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_2_combined_depths.tsv
-motif_combine_depths $r2 $r2_out
-
-r3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199725
-r3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_3_combined_depths.tsv
-motif_combine_depths $r3 $r3_out
-
-
-
-s1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199716_sim
-s1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_1_combined_depths.tsv
-motif_combine_depths $s1 $s1_out
-
-s2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199724_sim
-s2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_2_combined_depths.tsv
-motif_combine_depths $s2 $s2_out
-
-s3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199725_sim
-s3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_3_combined_depths.tsv
-motif_combine_depths $s3 $s3_out
-
-
-
+## bwa mem all mappings
+#r1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199716
+#r1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_1_combined_depths.tsv
+#combine_depths $r1 $r1_out
+#
+#r2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199724
+#r2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_2_combined_depths.tsv
+#combine_depths $r2 $r2_out
+#
+#r3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/3_samtools/SRR10199725
+#r3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/real_3_combined_depths.tsv
+#combine_depths $r3 $r3_out
+#
+#
+#
+#s1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199716_sim
+#s1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_1_combined_depths.tsv
+#combine_depths $s1 $s1_out
+#
+#s2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199724_sim
+#s2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_2_combined_depths.tsv
+#combine_depths $s2 $s2_out
+#
+#s3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/3_samtools/SRR10199725_sim
+#s3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_3_combined_depths.tsv
+#combine_depths $s3 $s3_out
+#
+#
+#
+#smsd1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199716_sim
+#smsd1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_1_combined_depths.tsv
+#combine_depths $smsd1 $smsd1_out
+#
+#smsd2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199724_sim
+#smsd2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_2_combined_depths.tsv
+#combine_depths $smsd2 $smsd2_out
+#
+#smsd3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/3_samtools/SRR10199725_sim
+#smsd3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/sim_mean_sd_3_combined_depths.tsv
+#combine_depths $smsd3 $smsd3_out
+#
+#
+## bwa mem matchings with motif sites only
+#r1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199716
+#r1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_1_combined_depths.tsv
+#motif_combine_depths $r1 $r1_out
+#
+#r2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199724
+#r2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_2_combined_depths.tsv
+#motif_combine_depths $r2 $r2_out
+#
+#r3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_real/5_samtools_motif_only/SRR10199725
+#r3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_real_3_combined_depths.tsv
+#motif_combine_depths $r3 $r3_out
+#
+#
+#
+#s1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199716_sim
+#s1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_1_combined_depths.tsv
+#motif_combine_depths $s1 $s1_out
+#
+#s2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199724_sim
+#s2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_2_combined_depths.tsv
+#motif_combine_depths $s2 $s2_out
+#
+#s3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim/5_samtools_motif_only/SRR10199725_sim
+#s3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_3_combined_depths.tsv
+#motif_combine_depths $s3 $s3_out
+#
+#
+#
 smsd1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/5_samtools_motif_only/SRR10199716_sim
 smsd1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_mean_sd_1_combined_depths.tsv
 motif_combine_depths $smsd1 $smsd1_out
@@ -104,4 +104,33 @@ motif_combine_depths $smsd2 $smsd2_out
 smsd3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_mean_sd/5_samtools_motif_only/SRR10199725_sim
 smsd3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_mean_sd_3_combined_depths.tsv
 motif_combine_depths $smsd3 $smsd3_out
+
+
+
+s100u1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_100_u/5_samtools_motif_only/SRR10199716_sim
+s100u1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_100u_1_combined_depths.tsv
+motif_combine_depths $s100u1 $s100u1_out
+
+s100u2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_100_u/5_samtools_motif_only/SRR10199724_sim
+s100u2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_100u_2_combined_depths.tsv
+motif_combine_depths $s100u2 $s100u2_out
+
+s100u3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_100_u/5_samtools_motif_only/SRR10199725_sim
+s100u3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_100u_3_combined_depths.tsv
+motif_combine_depths $s100u3 $s100u3_out
+
+
+
+s80c1=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_80_c/5_samtools_motif_only/SRR10199716_sim
+s80c1_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_80c_1_combined_depths.tsv
+motif_combine_depths $s80c1 $s80c1_out
+
+s80c2=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_80_c/5_samtools_motif_only/SRR10199724_sim
+s80c2_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_80c_2_combined_depths.tsv
+motif_combine_depths $s80c2 $s80c2_out
+
+s80c3=/pickett_flora/projects/read_simulation/analysis/snipen_2021/extract_features_sim_80_c/5_samtools_motif_only/SRR10199725_sim
+s80c3_out=/pickett_flora/projects/read_simulation/analysis/snipen_2021/depth_comparison/mo_sim_80c_3_combined_depths.tsv
+motif_combine_depths $s80c3 $s80c3_out
+
 
---
+++ analysis/snipen_2021/depth_comparison/run_many_sims_compare_depths.sh
@@ -0,0 +1,31 @@
+python3 /pickett_flora/projects/read_simulation/code/scripts/compare_samtools_depths.py -d \
+  mo_real_1_combined_depths.tsv \
+  mo_real_2_combined_depths.tsv \
+  mo_real_3_combined_depths.tsv \
+  mo_sim_1_combined_depths.tsv \
+  mo_sim_2_combined_depths.tsv \
+  mo_sim_3_combined_depths.tsv \
+  mo_sim_mean_sd_1_combined_depths.tsv \
+  mo_sim_mean_sd_2_combined_depths.tsv \
+  mo_sim_mean_sd_3_combined_depths.tsv \
+  mo_sim_100u_1_combined_depths.tsv \
+  mo_sim_100u_2_combined_depths.tsv \
+  mo_sim_100u_3_combined_depths.tsv \
+  mo_sim_80c_1_combined_depths.tsv \
+  mo_sim_80c_2_combined_depths.tsv \
+  mo_sim_80c_3_combined_depths.tsv -n \
+  mo_real_1 \
+  mo_real_2 \
+  mo_real_3 \
+  mo_sim_1 \
+  mo_sim_2 \
+  mo_sim_3 \
+  mo_sim_mean_sd_1 \
+  mo_sim_mean_sd_2 \
+  mo_sim_mean_sd_3 \
+  mo_sim_100u_1 \
+  mo_sim_100u_2 \
+  mo_sim_100u_3 \
+  mo_sim_80c_1 \
+  mo_sim_80c_2 \
+  mo_sim_80c_3
