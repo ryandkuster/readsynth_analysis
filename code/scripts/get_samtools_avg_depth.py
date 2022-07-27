c1d076d2b9861cb371d95c846280406af78eeae7
ryandkuster <rkuster@utk.edu>
2022-07-27 10:48:44 -0400

    added download genomes script

--- code/scripts/get_samtools_avg_depth.py
+++ code/scripts/get_samtools_avg_depth.py
@@ -11,4 +11,4 @@ sys.argv[2] is the filename to print to stdout
 df = pd.read_csv(sys.argv[1], sep="\t")
 colname = df.columns[2]
 df = df[df[colname] > 0]
-print(f'{sys.argv[2]}\t{df[colname].sum() / len(df)}\t{df[colname].median()}')
+print(f'{sys.argv[2]}\t{df[colname].mean()}\t{df[colname].median()}')
